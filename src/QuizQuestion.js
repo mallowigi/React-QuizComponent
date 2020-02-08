a992e8b6f14f242a049d2d080485504cd894333c
mallowigi <heliosaian@gmail.com>
2020-02-08 17:46:34 +0200

    shape

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      incorrectAnswer: false,
+    };
+  }
+
   handleClick(buttonText) {
+    this.setState({ incorrectAnswer: buttonText !== this.props.quiz_question.answer });
     if (buttonText === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler();
     }
@@ -25,6 +33,9 @@ class QuizQuestion extends Component {
             }
           </ul>
         </section>
+        {
+          this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null
+        }
       </main>
     );
   }
